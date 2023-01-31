afb4fcd4ae41bf3897c53953536bd0848d97f4f8
psong1 <paulsong29@gmail.com>
2023-01-30 19:28:44 -0500

    added function for changing background color based on time

--- Develop/script.js
+++ Develop/script.js
@@ -20,68 +20,85 @@
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-// });
+  var currentDate = dayjs().format('dddd') + " " + dayjs().format("Do MMM YYYY");
+  var currentTime = dayjs().format('h:mm:ss a');
+  var nineAm = $("#9");
+  var tenAm = $("#10");
+  var elevenAm = $("#11");
+  var twelvePm = $("#12");
+  var onePm = $("#13");
+  var twoPm = $("#14");
+  var threePm = $("#15");
+  var fourPm = $("#16");
+  var fivePm = $("#17");
+  
+  
+  function initPage() {
+
+    console.log("Current Hour " + hour);
+    var init9 = JSON.parse(localStorage.getItem("09:00 am"));
+    nineAm.val(init9);
+  
+    var init10 = JSON.parse(localStorage.getItem("10:00 am"))
+    tenAm.val(init10);
+    
+    var init11 = JSON.parse(localStorage.getItem("11:00 am"))
+    elevenAm.val(init11);
+    
+    var init12 = JSON.parse(localStorage.getItem("12:00 pm"))
+    twelvePm.val(init12);
+    
+    var init1 = JSON.parse(localStorage.getItem("01:00 pm"))
+    onePm.val(init1);
+    
+    var init2 = JSON.parse(localStorage.getItem("02:00 pm"))
+    twoPm.val(init2);
+    
+    var init3 = JSON.parse(localStorage.getItem("03:00 pm"))
+    threePm.val(init3);
+   
+    var init4 = JSON.parse(localStorage.getItem("04:00 pm"))
+    fourPm.val(init4);
+    
+    var init5 = JSON.parse(localStorage.getItem("05:00 pm"))
+    fivePm.val(init5);
+    
+    var init6 = JSON.parse(localStorage.getItem("06:00 pm"))
+    sixPm.val(init6);
+    
+    var init7 = JSON.parse(localStorage.getItem("07:00 pm"))
+    sevenPm.val(init7);
+  } 
+
 let now = dayjs(); console. log(now. format()); 
-let update = function () {
-  document.getElementById('currentDay').innerHTML = dayjs().format(
-    "dddd, MMM D, h:mm:ss a"
-  );
-};
-setInterval(update, 1000);
+var interval = setInterval(function() {
+  var currentTime = dayjs();
+  $('#currentDay').html(currentTime.format('YYYY MMMM DD') + ' '
+                      + currentTime.format('dddd')
+                       .substring(0,3).toUpperCase());
+  $('#currentDay').html(currentDate + currentTime.format('hh:mm:ss A'));
+}, 1000);
+
 
 function timeTracker() {
   //get current number of hours.
-  var timeNow = dayjs().hour();
+  var hour = dayjs().hour();
 
   // loop over time blocks
   $(".time-block").each(function () {
-      var blockTime = parseInt($(this).attr("id"));
-
-      // To check the time and add the classes for background indicators
-      if (blockTime < timeNow) {
-          $(this).removeClass("future");
-          $(this).removeClass("present");
-          $(this).addClass("past");
+      var timeTest = parseInt($(this).attr("id"));
+      hour = parseInt(hour);
+      console.log(timeTest);
+      console.log(hour);
+//      console.log(this);
+      if (hour > timeTest) {
+        $(this).addClass("past");
+      } else if (hour < timeTest) {
+        $(this).addClass("future");
+      } else {
+        $(this).addClass("present");
       }
-      else if (blockTime === timeNow) {
-          $(this).removeClass("past");
-          $(this).removeClass("future");
-          $(this).addClass("present");
-      }
-      else {
-          $(this).removeClass("present");
-          $(this).removeClass("past");
-          $(this).addClass("future");
-
-      }
-  })
+  });
 }
   
-    timeTracker();
-
-
-    // var timeNow = dayjs().hour();
-    // console.log(`The hour is ${timeNow}`);
-// $(document).ready(function () {
-//   let timeSlots = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-//   function renderPlans() {
-//     for (let i = 0; i <= timeSlots.length; i++) {
-//       $("#" + timeSlots[i]).val(localStorage.getItem(timeSlots[i]));
-//     }
-//   }
-//   renderPlans();
-
-//   $(".saveBtn").click(function () {
-//     let dataHour = $(this).attr("data-hour");
-//     let inputField = $("#" + dataHour).val();
-
-//     localStorage.setItem(dataHour, inputField);
-//     console.log(inputField);
-//   });
-// });
-
-
-
-// function setColor(element, color) {
-//   element.style.backgroundColor = color;
-// }
+  timeTracker();

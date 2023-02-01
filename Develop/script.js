15a322e982ec99fb62beaa0718423e881ab7fc52
psong1 <paulsong29@gmail.com>
2023-01-31 23:09:35 -0500

    added/removed comments and worked on README

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-// $(function () {
-
   let currentDate = dayjs().format('dddd') + " " + dayjs().format("Do MMM YYYY");
   let currentTime = dayjs().format('h:mm:ss a');
   let hour = dayjs().hour();
@@ -21,9 +16,7 @@
     start()
     timeTracker();
 
-    // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  // gets the user input that was saved in the local storage
   function start() {
     const start9 = JSON.parse(localStorage.getItem("09:00 am"));
     nineAm.val(start9);
@@ -53,40 +46,21 @@
     fivePm.val(start17);
   } 
   
- // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  // Buttons (save to Local Storage)
+  // button to save to local storage; uses the class of the time block's div and text area
   $(".saveBtn").on("click", function(){
     userInput = $(this).siblings(".description").val().trim();
-    console.log(userInput);
-    hourSpan = $(this).siblings(".hour").text().trim();
-    console.log(hourSpan);
-    localStorage.setItem(hourSpan, JSON.stringify(userInput));
+    hourBlock = $(this).siblings(".hour").text().trim();
+    localStorage.setItem(hourBlock, JSON.stringify(userInput));
   })
 });
 
-
-
-  // TODO: Add code to display the current date in the header of the page.
 // gets the id from the html and adds the current date and time in the specific format using day.js
-let now = dayjs(); console. log(now. format()); 
 let update = function () {
   document.getElementById("currentDay").innerHTML = dayjs().format(
     "dddd, MMM D, h:mm:ss a"
   );
 };
 setInterval(update, 1000);
-
-
- // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
 
   // gets the current hour and loops over time-blocks to add the right class relative to the current hour
 function timeTracker() {
--- README.md
+++ README.md
@@ -2,4 +2,16 @@
 
 ## Description
 
-This simple work day calendar allows you to schedule and save specific events from 9 AM to 5 PM. These events are logged in the local storage then set in the corresponding time block. Day.js is used to provide the current date and time, and the time blocks change color based on the current hour. It does this by parsing the integers of the time block and the current hour, and compares those values to provide the color. 
+This simple work day calendar allows you to schedule and save specific events from 9 AM to 5 PM. These events are logged in the local storage then set in the corresponding time block. Day.js is used to provide the current date and time in the header, and the time blocks change color based on the current hour. It does this by parsing the integers of the time block and the current hour, and compares those values to provide the color. 
+
+## Preview of Deployed Application
+
+![img](./Assets/images/scheduler-past.jpg)
+
+* This preview of the application shows the scheduler with the date and time in the header. The background color of the time blocks are set to gray when the hour is in the past. If the time block matches the current hour, the time block will be red. If the time block is in the future relative to the current hour, the time block will be green.
+
+![img]()
+
+# Deployed Page URL
+
+*
